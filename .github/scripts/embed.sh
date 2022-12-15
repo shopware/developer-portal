4f450573537621b2e2876ee6fc7468fe61c9f495
Bojan Rajh <b.rajh@shopware.com>
2022-12-15 11:01:55 +0100

    Mount icon kit

--- .github/scripts/embed.sh
+++ .github/scripts/embed.sh
@@ -12,7 +12,7 @@ sh ./.github/scripts/clone.sh bojanrajh:${GITLAB_FRONTENDS_ACCESS_KEY}@gitlab.sh
 #sh ./.github/scripts/clone.sh git@github.com/shopware/admin-extension-sdk.git main docs/docs/guide docs/resources/admin-extension-sdk
 #sh ./.github/scripts/clone.sh git@github.com/shopware/store-api-reference.git main docs docs/resources/store-api-reference
 #sh ./.github/scripts/clone.sh git@github.com/shopware/admin-api-reference.git main docs docs/resources/admin-api-reference
-#sh ./.github/scripts/clone.sh git@github.com/shopware/meteor-icon-kit.git main docs resources/meteor-icon-kit
+sh ./.github/scripts/clone.sh git@github.com/shopware/meteor-icon-kit.git main docs resources/meteor-icon-kit
 
 echo "\nClone versioned repositories"
 #sh ./.github/scripts/clone.sh git@github.com/shopware/docs.git v6.4 . v/6.4/docs
