8edf389cdd79ad8be8af100970b9533230f69016
Bojan Rajh <b.rajh@shopware.com>
2022-12-21 11:21:14 +0100

    Skip icons

--- .github/scripts/embed.sh
+++ .github/scripts/embed.sh
@@ -20,8 +20,8 @@ sh ./.github/scripts/clone.sh git@github.com/shopware/admin-extension-sdk.git ${
 #sh ./.github/scripts/clone.sh git@github.com/shopware/store-api-reference.git main docs docs/resources/store-api-reference
 #sh ./.github/scripts/clone.sh git@github.com/shopware/admin-api-reference.git main docs docs/resources/admin-api-reference
 
-echo "\nBuilding icons"
-sh ./.github/scripts/clone.sh git@github.com/${ORG_METEOR_ICON_KIT}/meteor-icon-kit.git ${BRANCH_METEOR_ICON_KIT} docs resources/meteor-icon-kit meteor-icon-kit
+#echo "\nBuilding icons"
+#sh ./.github/scripts/clone.sh git@github.com/${ORG_METEOR_ICON_KIT}/meteor-icon-kit.git ${BRANCH_METEOR_ICON_KIT} docs resources/meteor-icon-kit meteor-icon-kit
 
 echo "\nClone versioned repositories"
 #sh ./.github/scripts/clone.sh git@github.com/shopware/docs.git v6.4 . v/6.4/docs
