6152a753006b1c7ee6c2d5afb8ba017cb88ae64a
Bojan Rajh <b.rajh@shopware.com>
2023-01-05 17:00:12 +0100

    Log result

--- __tests__/cli/command/embed.test.ts
+++ __tests__/cli/command/embed.test.ts
@@ -49,6 +49,8 @@ describe('cli embed', async () => {
 
         const result = await docsCli(['embed'], sandbox.cwd, {...timeout.high/*, env: secrets*/});
 
+        console.log(result);
+
         expect(result.stdout).toContain('Embedding repositories');
 
         expect(result.stdout).toContain('Embedding shopware/frontends');
