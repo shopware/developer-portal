653490d1cd69a4fca1bb1032b9c82e1b90dc87cd
Bojan Rajh <b.rajh@shopware.com>
2023-03-07 09:35:11 +0100

    Readme

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -1,30 +1,22 @@
 #! /usr/bin/env bash
 
-echo "Installing (p)npm dependencies"
+echo "POST-INSTALL: Installing (p)npm dependencies"
 pnpm i
 
-echo "Building CLI"
+echo "POST-INSTALL: Building CLI"
 pnpm cli:build
 
-echo "CLI"
-./docs-cli || echo "1"
+echo "POST-INSTALL: Showing CLI"
+./docs-cli || echo ""
 
-# doesn't work because of org access
-# https://github.com/codespaces-contrib/codespaces-multi-repo
-# Remove the default credential helper
-# Add one that just uses secrets available in the Codespace
-#echo "Changing git credentials"
-#sudo sed -i -E 's/helper =.*//' /etc/gitconfig
-#git config --global credential.helper '!f() { sleep 1; echo "username=${GITHUB_USER}"; echo "password=${GH_TOKEN}"; }; f'
-
-echo "Set up SSH"
+echo "POST-INSTALL: Set up SSH"
 mkdir ~/.ssh/
 echo $GH_SWAG_SSH_PRIVATE_KEY | base64 --decode > ~/.ssh/id_ed25519
 chmod 600 ~/.ssh/id_ed25519 
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
-echo "Cloning repos to ./repos/"
+echo "POST-INSTALL: Cloning repos to ./repos/"
 ./docs-cli git
 
-echo "Running dev server"
+echo "POST-INSTALL: Running dev server"
 pnpm dev
--- README.md
+++ README.md
@@ -36,6 +36,8 @@ _Note: This repository does not include layouts, templates and other components 
 See [QUICK START](./QUICK-START.md) for more info on how to start using Developer Portal, including some basic examples
 on how to embed and manage docs repos, and preview the dev and prod builds.
 
+For a ready-to-code example, see [DevContainer](./DEV-CONTAINER.md).
+
 # Table of contents
 
 | Section                              | Description                                                                               |
