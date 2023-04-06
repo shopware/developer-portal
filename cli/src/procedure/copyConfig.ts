abf3926df3917dcc7f78a496b5ace2662518e93c
Bojan Rajh <b.rajh@shopware.com>
2023-04-06 14:28:11 +0200

    Copy multiple configs

--- cli/src/procedure/copyConfig.ts
+++ cli/src/procedure/copyConfig.ts
@@ -3,11 +3,25 @@ import {output} from "../output";
 import {run} from "../helpers";
 
 export const copyConfig = async (src: string, dst: string) => {
-    const docsConfig = '.github/scripts/docs.yml';
-    if (!fs.existsSync(`${src}/${docsConfig}`) || !fs.lstatSync(`${src}/${docsConfig}`).isFile()) {
-        return;
+    const configs = [
+        {
+            src: '.github/scripts/docs.yml',
+            dst: 'docs.yml',
+            message: 'Copying external config',
+        },
+        {
+            src: '.gitbook.yaml',
+            dst: '.gitbook.yaml',
+            message: 'Copying gitbook config',
+        }
+    ];
+
+    for (const config of configs) {
+        if (!fs.existsSync(`${src}/${config.src}`) || !fs.lstatSync(`${src}/${config.src}`).isFile()) {
+            continue;
+        }
+
+        output.notice(config.message);
+        await run('cp', [`${src}/${config.src}`, `${dst}/${config.dst}`], {dir: src});
     }
-    
-    output.notice('Copying external config');
-    await run('cp', [`${src}/${docsConfig}`, `${dst}/docs.yml`], {dir: src});
 }
