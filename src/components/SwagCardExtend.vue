d4cb2090324e2ea7655e7d469e9ed549ab5c13f9
Bojan Rajh <b.rajh@shopware.com>
2023-07-06 09:25:21 +0200

    Missing component

---
+++ src/components/SwagCardExtend.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="SwagExtendShopware_card c-flat-card">
+    <div class="SwagExtendShopware_heading"><slot name="title" /></div>
+    <div class="leading-6"><slot name="description" /></div>
+    <div class="SwagExtendShopware_availabilities mt-4 leading-6">
+      <span class="SwagExtendShopware_availability" v-for="availability in availabilities">{{ availability }}</span>
+    </div>
+    <p><slot name="link" /></p>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  availabilities: {
+    type: Array,
+  }
+});
+</script>
