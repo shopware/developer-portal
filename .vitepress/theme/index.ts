9dac0fe27f88cbc24fb479ee1f37884f4e509430
Bojan Rajh <b.rajh@shopware.com>
2023-07-12 16:55:00 +0200

    Fix upstream

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,11 @@
 import {SWAGTheme} from "vitepress-shopware-docs";
-import {h} from 'vue'
 import SwagRadialBg from "../../src/components/SwagRadialBg.vue";
 
 export default {
     ...SWAGTheme({
         slots: {
             'layout-top': [
-                h(SwagRadialBg),
+                SwagRadialBg,
             ]
         }
     }),
