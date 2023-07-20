828591382a1d9b3783c23108b6e655d8c51c3b82
Su <112690947+sushmangupta@users.noreply.github.com>
2023-07-20 11:05:28 +0200

    textual changes (#74)

--- src/components/SwagGetToKnow.vue
+++ src/components/SwagGetToKnow.vue
@@ -7,22 +7,22 @@
 
       <SwagCard page="/docs/concepts/framework/">
         <template #title>What is Shopware?</template>
-        <template #description>Learn about Shopware, its features, benefits.</template>
+        <template #description>Learn about Shopware capabilities, framework, architecture and more.</template>
       </SwagCard>
 
       <SwagCard page="#Extend">
         <template #title>How to start with Shopware?</template>
-        <template #description>Get to know which ways you can get involved with Shopware as a developer.</template>
+        <template #description>Ways you can get involved with Shopware as a developer.</template>
       </SwagCard>
 
       <SwagCard page="/docs/guides/installation/">
         <template #title>How to install Shopware?</template>
-        <template #description>Set up Shopware on your local machine or use a developer sandbox from the cloud.</template>
+        <template #description>Based on your requirements and resources, setup your store.</template>
       </SwagCard>
 
       <SwagCard page="/docs/products/extensions/migration-assistant/">
         <template #title>How to migrate to Shopware?</template>
-        <template #description>Learn easy ways to migrate to Shopware irrespective of your old instance.</template>
+        <template #description>Ways to migrate to Shopware irrespective of your old instance.</template>
       </SwagCard>
 
     </div>
