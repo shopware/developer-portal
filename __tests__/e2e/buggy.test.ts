23b8a595caf0770d619ca4df47ced3847a7b0587
Bojan Rajh <b.rajh@shopware.com>
2023-07-21 16:55:07 +0200

    Add buggy test

---
+++ __tests__/e2e/buggy.test.ts
@@ -0,0 +1,41 @@
+describe('catch some bugs', async () => {
+    beforeAll(async () => {
+        await goto('/')
+    })
+
+    /*test('has no console errors', async () => {
+        page.on('console', msg => console.log(msg.text()));
+    })*/
+
+    test('has custom components on renavigation', async () => {
+        const expectSwitcher = async (num: number, type: string) => expect(await page.locator('.SwagSidebarVersionSwitcher').count(), type).toEqual(num, type);
+        const expectFooter = async (type) => expect(await page.locator('.SwagFooter').count(), type).toEqual(1, type);
+
+        const fromTheHomepage = async(type: string) => {
+            await page.locator('.SwagGetToKnow .SwagCard:first-child').click();
+            await expectSwitcher(1, type);
+            await expectFooter(type);
+        }
+
+        await goto('/');
+
+        await expectSwitcher(0, 'first');
+
+        // navigate from the homepage
+        await fromTheHomepage('initial');
+
+        // navigate to the homepage
+        await page.locator('a.VPNavBarTitle').click();
+        await expectSwitcher(0, 'second');
+        await expectFooter('second');
+
+        // navigate from the homepage
+        await fromTheHomepage('last');
+    });
+
+    test('content count', async () => {
+        const contentLocator = page.locator('.VPContent')
+
+        expect(await contentLocator.count()).toEqual(1)
+    })
+})
