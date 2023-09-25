eaf3e66d21ebd7004b3f1a88f7fd22ec90ef8040
Bojan Rajh <b.rajh@shopware.com>
2023-09-25 10:18:29 +0200

    fix: migrate to developer-portal-vitepress

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,5 @@
 import {SWAGTheme} from "vitepress-shopware-docs";
 import SwagRadialBg from "../../src/components/SwagRadialBg.vue";
-import { onMounted, watch, nextTick } from 'vue';
-import { useRoute } from 'vitepress';
-import mediumZoom from 'medium-zoom';
-
-import './index.css'; 
 
 export default {
     ...SWAGTheme({
@@ -14,17 +9,4 @@ export default {
             ]
         }
     }),
-    setup() {
-        const route = useRoute();
-        const initZoom = () => {
-            mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
-          };
-        onMounted(() => {
-            initZoom();
-        });
-        watch(
-            () => route.path,
-            () => nextTick(() => initZoom())
-          );
-      },
 }
