9a330cb4736eb743de3122ca3629d3d6a96c3de6
Bojan Rajh <b.rajh@shopware.com>
2023-09-27 14:28:51 +0200

    fix: switch to main

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -107,7 +107,7 @@ const embeds: SwagEmbedsConfig[] = [
     points: {
       '/docs/v6.4/': 'v6.4',
       '/docs/v6.3/': 'v6.3.0',
-      '/docs/': 'master',
+      '/docs/': 'main',
     },
     folder: '.',
   },
