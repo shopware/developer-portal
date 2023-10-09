c4a7df5d5d5b0f369adce44f5b071624f5ea62ce
Bojan Rajh <b.rajh@shopware.com>
2023-10-09 13:49:54 +0200

    Add medium-zoom

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,5 +1,10 @@
 import {SWAGTheme} from "vitepress-shopware-docs";
 import SwagRadialBg from "../../src/components/SwagRadialBg.vue";
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+import mediumZoom from 'medium-zoom';
+
+import './index.css'; 
 
 export default {
     ...SWAGTheme({
@@ -9,4 +14,17 @@ export default {
             ]
         }
     }),
+    setup() {
+        const route = useRoute();
+        const initZoom = () => {
+            mediumZoom('.main img', { background: 'var(--vp-c-bg)' });
+          };
+        onMounted(() => {
+            initZoom();
+        });
+        watch(
+            () => route.path,
+            () => nextTick(() => initZoom())
+          );
+      },
 }
