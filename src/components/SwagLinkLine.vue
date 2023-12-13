5126d80fed4bd664066c99b91010ed4c03ac80c2
Bojan Rajh <b.rajh@shopware.com>
2023-12-13 16:23:19 +0100

    feat: switch meteor icon kit to main

---
+++ src/components/SwagLinkLine.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="c-link-line">
+    <a v-if="file"
+       :href="`https://github.com/${repo}/edit/${branch}/${file}`">
+      <SwagIcon icon="github"/>
+      Edit this page on GitHub</a>
+    <a href="https://stackoverflow.com/questions/ask?tags=shopware">
+      <SwagIcon icon="stackoverflow"/>
+      Ask question on Stackoverflow</a>
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  file: {
+    required: false,
+  },
+  repo: {
+    required: false,
+    default: 'shopware/developer-portal',
+  },
+  branch: {
+    required: false,
+    default: 'main',
+  },
+});
+</script>
