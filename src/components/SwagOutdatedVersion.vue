4b36031dce618b66cd2ff97d9d5d7575e6efee13
Bojan Rajh <b.rajh@shopware.com>
2024-12-11 11:16:25 +0100

    fix: border

--- src/components/SwagOutdatedVersion.vue
+++ src/components/SwagOutdatedVersion.vue
@@ -1,13 +1,13 @@
 <template>
     <PageRef
         v-if="route.path.startsWith('/docs/v6.7')"
-        class="mb-8"
+        class="SwagOutdatedVersion my-8"
         page="/docs/"
         title="You are viewing next version (v6.7) of the documentation."
         sub="Click here to switch to the stable (v6.6) version, or use the version switcher on the left to navigate between versions." />
     <PageRef
-        v-if="route.path.startsWith('/docs/v6.')"
-        class="mb-8"
+        v-else-if="route.path.startsWith('/docs/v6.')"
+        class="SwagOutdatedVersion my-8"
         page="/docs/"
         title="You are viewing outdated version of the documentation."
         sub="Click here to switch to the stable (v6.6) version, or use the version switcher on the left to navigate between versions." />
@@ -18,3 +18,9 @@ import { useRoute } from "vitepress";
 
 const route = useRoute()
 </script>
+
+<style lang="scss">
+.SwagOutdatedVersion {
+    outline: 3px var(--sw-c-blue-vivacious) solid;
+}
+</style>
