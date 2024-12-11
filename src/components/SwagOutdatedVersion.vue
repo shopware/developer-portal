f5d58b82a1a6ae4e2d0afff82c0be05ec16b5893
Bojan Rajh <b.rajh@shopware.com>
2024-12-11 13:04:36 +0100

    feat: bump deps

--- src/components/SwagOutdatedVersion.vue
+++ src/components/SwagOutdatedVersion.vue
@@ -2,21 +2,23 @@
     <PageRef
         v-if="route.path.startsWith('/docs/v6.7')"
         class="SwagOutdatedVersion my-8"
-        page="/docs/"
+        :page="canonicalUrl"
         title="You are viewing next version (v6.7) of the documentation."
-        sub="Click here to switch to the stable (v6.6) version, or use the version switcher on the left to navigate between versions." />
+        sub="Click here to switch to the stable version (v6.6), or use the version switcher on the left to navigate between versions." />
     <PageRef
         v-else-if="route.path.startsWith('/docs/v6.')"
         class="SwagOutdatedVersion my-8"
-        page="/docs/"
+        :page="canonicalUrl"
         title="You are viewing outdated version of the documentation."
-        sub="Click here to switch to the stable (v6.6) version, or use the version switcher on the left to navigate between versions." />
+        sub="Click here to switch to the stable version (v6.6), or use the version switcher on the left to navigate between versions." />
 </template>
 
 <script setup lang="ts">
 import { useRoute } from "vitepress";
 
 const route = useRoute()
+const [ emptyPrefix, docs, version, ...rest ] = route.path.split('/')
+const canonicalUrl = `/${docs}/${rest.join('/')}`
 </script>
 
 <style lang="scss">
