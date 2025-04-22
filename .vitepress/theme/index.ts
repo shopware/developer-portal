9fb75807b5fc879fc65047443f81b6b4c44515f1
Bojan Rajh <b.rajh@shopware.com>
2025-04-22 16:55:53 +0200

    feat: ribbon

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import {SWAGTheme} from "vitepress-shopware-docs";
 import SwagRadialBg from "../../src/components/SwagRadialBg.vue";
 import SwagOutdatedVersion from "../../src/components/SwagOutdatedVersion.vue";
+import SwagRibbon from "../../src/components/SwagRibbon.vue";
 //import SwagCopilotSidebar from "../../src/components/copilot/SwagCopilotSidebar.vue";
 import SwagCopilotMenu from "../../node_modules/vitepress-shopware-docs/src/shopware/components/copilot/SwagCopilotMenu.vue";
 import "./style.scss";
@@ -8,6 +9,9 @@ import "./style.scss";
 export default {
     ...SWAGTheme({
         slots: {
+            'layout-top': [
+              SwagRibbon,
+            ],
             'layout-bottom': [
                 SwagRadialBg,
                 //SwagCopilotSidebar,
---
+++ src/components/SwagRibbon.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="SwagRibbon">
+        <SwagIcon icon="party-horn" type="solid" />
+        <span v-html="text"></span>
+        <SwagIcon icon="party-horn" type="solid" />
+    </div>
+</template>
+
+<script setup>
+const texts = [
+    'Shopware 6.7 Release Candidate 2 – <a href="https://www.shopware.com/en/news/shopware-6-7-release-candidate-2/" target="_blank">now available</a>',
+    'Checkout new community home - <a href="https://hub.shopware.com/" target="_blank">Community Hub</a>',
+];
+const text = texts[Math.floor(Math.random() * texts.length)];
+</script>
+
+<style lang="scss">
+.SwagRibbon {
+    @apply text-xs font-medium text-center px-4 py-2 fixed left-0 right-0 top-0 bg-[--sw-c-blue-vivacious-50] dark:bg-[--sw-c-gray-dark-700] flex gap-2 items-center justify-center;
+    z-index: var(--vp-z-index-nav);
+    a {
+        @apply underline;
+    }
+    .SwagIcon {
+        @apply text-[--sw-c-blue-vivacious-900];
+    }
+}
+
+#app:has(.SwagRibbon) {
+    header.VPNav {
+        @apply mt-8;
+    }
+
+    .VPNavScreen {
+        top: calc(var(--vp-nav-height, 0px) + var(--vp-layout-top-height, 0px) + 1rem);
+    }
+}
+
+</style>
