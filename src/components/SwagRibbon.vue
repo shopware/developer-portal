1b9dc98bfd8a711a6f0c39ed1a32fe1f1fdaa21a
Bojan Rajh <b.rajh@shopware.com>
2025-04-24 12:07:45 +0200

    fix: client-only rendering

--- src/components/SwagRibbon.vue
+++ src/components/SwagRibbon.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="SwagRibbon">
-        <SwagIcon icon="party-horn" type="solid" />
-        <span v-html="text"></span>
-        <SwagIcon icon="party-horn" type="solid" />
-    </div>
+    <ClientOnly>
+        <div class="SwagRibbon">
+            <SwagIcon icon="party-horn" type="solid" />
+            <span v-html="text"></span>
+            <SwagIcon icon="party-horn" type="solid" />
+        </div>
+    </ClientOnly>
 </template>
 
 <script setup>
 const texts = [
     'Shopware 6.7 Release Candidate 2 – <a href="https://www.shopware.com/en/news/shopware-6-7-release-candidate-2/" target="_blank">now available</a>',
-    'Checkout new community home - <a href="https://hub.shopware.com/" target="_blank">Community Hub</a>',
+    'Checkout <span class="hidden sm:inline">our</span> new community home - <a href="https://hub.shopware.com/" target="_blank">Community Hub</a>',
 ];
 const text = texts[Math.floor(Math.random() * texts.length)];
 </script>
 
 <style lang="scss">
 .SwagRibbon {
-    @apply text-xs font-medium text-center px-4 py-2 fixed left-0 right-0 top-0 bg-[--sw-c-blue-vivacious-50] dark:bg-[--sw-c-gray-dark-700] flex gap-2 items-center justify-center;
+    @apply text-xs font-medium text-center p-2 fixed left-0 right-0 top-0 bg-[--sw-c-blue-vivacious-50] dark:bg-[--sw-c-gray-dark-700] flex gap-2 items-center justify-center;
     z-index: var(--vp-z-index-nav);
     a {
         @apply underline;
