a7d1010c7d2767c635fee48a4dde60df1e6e34be
Bojan Rajh <b.rajh@shopware.com>
2025-06-18 20:09:34 +0200

    fix: version notice

--- src/components/SwagOutdatedVersion.vue
+++ src/components/SwagOutdatedVersion.vue
@@ -1,18 +1,18 @@
 <template>
     <PageRef
-        v-if="route.path.startsWith('/docs/v6.7')"
+        v-if="route.path.startsWith('/docs/v6.8')"
         class="SwagOutdatedVersion my-8"
         :key="`${canonicalUrl}-next`"
         :page="canonicalUrl"
-        title="You are viewing the next version (v6.7) of the documentation."
-        sub="Click here to switch to the stable version (v6.6), or use the version switcher on the left to navigate between versions." />
+        title="You are viewing the next version (v6.8) of the documentation."
+        sub="Click here to switch to the stable version (v6.7), or use the version switcher on the left to navigate between versions." />
     <PageRef
         v-else-if="route.path.startsWith('/docs/v6.')"
         class="SwagOutdatedVersion my-8"
         :key="`${canonicalUrl}-outdated`"
         :page="canonicalUrl"
         title="You are viewing an outdated version of the documentation."
-        sub="Click here to switch to the stable version (v6.6), or use the version switcher on the left to navigate between versions." />
+        sub="Click here to switch to the stable version (v6.7), or use the version switcher on the left to navigate between versions." />
 </template>
 
 <script setup lang="ts">
