292dd006f41710584e408204e00947303e7a0d22
Bojan Rajh <b.rajh@shopware.com>
2026-05-21 09:41:11 +0200

    fix: ignore versioned deadlinks

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -291,7 +291,7 @@ export default await withExternals(withMermaid(defineConfigWithTheme<ThemeConfig
     (url, file) => {
       // skip broken links in versions
       if (file?.includes('docs/v6.5') || file?.includes('docs/v6.6')) {
-        return false
+        return true
       }
 
       try {
