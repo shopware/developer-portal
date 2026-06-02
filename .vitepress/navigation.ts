e2930373f96c8418fdeb5872a66ed0b6c591c3f2
Micha <m.hobert@shopware.com>
2026-06-02 10:56:38 +0200

    change/content

--- .vitepress/navigation.ts
+++ .vitepress/navigation.ts
@@ -138,11 +138,11 @@ const navigation = buildSidebarNav('./src/', [
                         link: '/docs/products/tools/cli/',
                         activeMatch: '^\\/docs\\/products\\/tools\\/cli\\/',
                     },
-                    /*{
+                    {
                         text: 'Shopware MCP',
-                        // link: '/docs/products/tools/mcp/',
-                        link: '/docs/guides/development/tooling/mcp/',
-                    },*/
+                        link: '/docs/products/tools/mcp-server/',
+                        activeMatch: '^\\/docs\\/products\\/tools\\/mcp-server\\/',
+                    },
                 ]
             },
         ],
